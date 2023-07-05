b3592178b95b03d476a686c94c56dff000b82039
CircuitSacul <circuitsacul@icloud.com>
2023-07-05 15:19:46 -0400

    only respond to humans and commands

--- coderunner/plugins/events.py
+++ coderunner/plugins/events.py
@@ -1,6 +1,7 @@
 import crescent
 import hikari
 
+from coderunner import models
 from coderunner.app import Plugin
 from coderunner.plugins.instance import Instance
 
@@ -10,8 +11,27 @@ plugin = Plugin()
 @plugin.include
 @crescent.event
 async def on_message(event: hikari.MessageCreateEvent) -> None:
+    if not event.is_human:
+        return None
+    if not (ct := event.message.content):
+        return None
+    if cmd := ct.lstrip("io/"):
+        try:
+            cmd = cmd.split(" ", 1)[0]
+        except KeyError:
+            return None
+        match cmd:
+            case "run":
+                action = models.Action.RUN
+            case "asm":
+                action = models.Action.ASM
+            case unknown:
+                await event.message.respond(f"Unknown command {unknown}.", reply=True)
+                return None
+
     instance = await Instance.from_original(event.message, event.author_id)
     if not instance:
         return
 
+    instance.action = action
     await instance.execute()
