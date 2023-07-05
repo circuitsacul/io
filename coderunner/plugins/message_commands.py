e842d3b4762f530005e168e2cb544dd362346c17
CircuitSacul <circuitsacul@icloud.com>
2023-07-05 16:48:21 -0400

    add message command for instance creation

---
+++ coderunner/plugins/message_commands.py
@@ -0,0 +1,19 @@
+import crescent
+import hikari
+
+from coderunner.app import Plugin
+
+from .instance import Instance
+
+plugin = Plugin()
+
+@plugin.include
+@crescent.message_command(name="Create Instance")
+async def create_instance(ctx: crescent.Context, msg: hikari.Message) -> None:
+    inst = await Instance.from_original(msg, ctx.user.id)
+    if not inst:
+        await ctx.respond("No code blocks to run could be found.", ephemeral=True)
+        return
+
+    await ctx.respond("Instance created.", ephemeral=True)
+    await inst.execute()
