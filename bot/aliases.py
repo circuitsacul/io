3e9652074761d3542d91d6d95b08e258cd50bda2
Lunarmagpie <bambolambo0@gmail.com>
2023-07-06 16:15:13 -0400

    add ci

--- bot/aliases.py
+++ bot/aliases.py
@@ -1,7 +1,7 @@
 _ALIASES = {
     "python": ["py", "python3", "py3"],
     "python2": ["py2"],
-    "c++": ["cpp", "cppp", "cp", "c+"],
+    "c++": ["cpp", "cp", "c+"],
     "csharp": ["c#"],
     "rust": ["rs"],
 }
---
+++ noxfile.py
@@ -0,0 +1,32 @@
+import nox
+
+SRC_FILES = [
+    "bot",
+    "noxfile.py",
+]
+
+
+def install_poetry_groups(session: nox.Session, *groups: str):
+    session.run("poetry", "install", f"--with={','.join(groups)}")
+
+
+@nox.session
+def typecheck(session: nox.Session):
+    install_poetry_groups(session, "typing")
+    session.run("mypy", *SRC_FILES)
+
+
+@nox.session
+def lint(session: nox.Session):
+    install_poetry_groups(session, "linting")
+    session.run("black", "--check", *SRC_FILES)
+    session.run("ruff", "check", *SRC_FILES)
+    session.run("codespell", *SRC_FILES)
+
+
+@nox.session
+def fix(session: nox.Session):
+    install_poetry_groups(session, "linting")
+    session.run("black", *SRC_FILES)
+    session.run("ruff", "check", "--fix", *SRC_FILES)
+    session.run("codespell", "-w", "-i2", *SRC_FILES)
