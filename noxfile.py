3419f7bcd62bc96dc50490d0f6257621b8aac311
Lunarmagpie <bambolambo0@gmail.com>
2023-07-06 16:30:30 -0400

    use .

--- noxfile.py
+++ noxfile.py
@@ -1,10 +1,5 @@
 import nox
 
-SRC_FILES = [
-    "bot",
-    "noxfile.py",
-]
-
 
 def install_poetry_groups(session: nox.Session, *groups: str) -> None:
     session.run("poetry", "install", f"--with={','.join(groups)}")
@@ -13,20 +8,20 @@ def install_poetry_groups(session: nox.Session, *groups: str) -> None:
 @nox.session
 def typecheck(session: nox.Session) -> None:
     install_poetry_groups(session, "typing")
-    session.run("mypy", *SRC_FILES)
+    session.run("mypy", ".")
 
 
 @nox.session
 def lint(session: nox.Session) -> None:
     install_poetry_groups(session, "linting")
-    session.run("black", "--check", *SRC_FILES)
-    session.run("ruff", "check", *SRC_FILES)
-    session.run("codespell", *SRC_FILES)
+    session.run("black", "--check", ".")
+    session.run("ruff", "check", ".")
+    session.run("codespell", ".")
 
 
 @nox.session
 def fix(session: nox.Session) -> None:
     install_poetry_groups(session, "linting")
-    session.run("black", *SRC_FILES)
-    session.run("ruff", "check", "--fix", *SRC_FILES)
-    session.run("codespell", "-w", "-i2", *SRC_FILES)
+    session.run("black", ".")
+    session.run("ruff", "check", "--fix", ".")
+    session.run("codespell", "-w", "-i2", ".")
