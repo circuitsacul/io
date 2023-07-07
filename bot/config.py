bcaf3ee37d1e08c4cbf4ce523fdade8201995a1f
Lunarmagpie <bambolambo0@gmail.com>
2023-07-06 23:21:23 -0400

    changes

--- bot/config.py
+++ bot/config.py
@@ -7,9 +7,14 @@ import dotenv
 @dataclasses.dataclass
 class Config:
     TOKEN: str
-    PISTON_URL: str
-    GODBOLT_URL: str
+    PISTON_URL: str = "https://emkc.org/api/v2/piston/"
+    GODBOLT_URL: str = "https://godbolt.org/api/"
 
 
 env_vars: dict[str, str] = t.cast(dict[str, str], dotenv.dotenv_values())
-CONFIG = Config(**env_vars)
+try:
+    CONFIG = Config(**env_vars)
+except TypeError as e:
+    raise Exception(
+        "You have an error in your .env file. Check the README for more info."
+    ) from e
