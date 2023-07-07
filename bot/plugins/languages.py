98afc3a84ff3bd6676ab59d96f172787967f3063
Lunarmagpie <bambolambo0@gmail.com>
2023-07-07 02:43:22 -0400

    add languages command

---
+++ bot/plugins/languages.py
@@ -0,0 +1,18 @@
+import crescent
+from bot.app import Plugin
+import hikari
+from bot.constants import EMBED_COLOR
+
+plugin = Plugin()
+
+
+@plugin.include
+@crescent.command
+async def languages(ctx: crescent.Context) -> None:
+    lang_names = list(
+        f"`{runtime}`" for runtime in plugin.model.manager.runtimes.run if runtime
+    )
+    embed = hikari.Embed(
+        title="Supported Languages", description=",".join(lang_names), color=EMBED_COLOR
+    )
+    await ctx.respond(embed=embed)
